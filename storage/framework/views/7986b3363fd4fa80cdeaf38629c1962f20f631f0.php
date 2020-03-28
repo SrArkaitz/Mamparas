2f32bc75246b85a9857d3de0a6c146121dba16fe
Arkaitz <97agalisteo@sanferminikastola.org>
2020-03-28 17:41:18 +0100

    .

---
+++ storage/framework/views/7986b3363fd4fa80cdeaf38629c1962f20f631f0.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<h1>Hola</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/vagrant/Code/resources/views/index.blade.php ENDPATH**/ ?>
